89ad280ea7a65781c01ff4ba698d5dd28a3c035b
jorge.lopez <jorge.lopezrodriguez.23@campusfp.es>
2024-05-16 14:46:28 +0200

    capa visual mostar en Listview

--- src/main/java/com/empresa/javafxmongocrud100/HelloController.java
+++ src/main/java/com/empresa/javafxmongocrud100/HelloController.java
@@ -1,14 +1,35 @@
 package com.empresa.javafxmongocrud100;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import org.bson.Document;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ListView lv_datos;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-}
+
+    @FXML
+    protected void mostrar (){
+        String url="mongodb+srv://administrador:Abc123456@cluster0.qhfs8na.mongodb.net/";
+        MongoClient mongoClient = MongoClients.create(url);
+        MongoDatabase database = mongoClient.getDatabase("actividad17");
+        MongoCollection<Document> collection = database.getCollection("administrador");
+
+        for (Document doc : collection.find()) {
+
+            lv_datos.getItems().add(doc);
+        }
+    }
+} // cierra la clase
